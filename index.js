429ef5e31cf37931ec9b1e344521f32457be5b6f
bdemers1055 <bdemers1055@gmail.com>
2018-06-13 09:05:09 -0400

    added dates and temporary file clearing

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const fs = require('fs'); //this tells node that we need this module (set of fun
 
 console.log("welcome to remind me"); // this tells node we need this module (se of functions and attributes)
 
+const reminderFile = "reminders.txt";
+
+fs.unlinkSync(reminderFile);
+
 const reminders = [
     "get some milk", //0
     "write a song", //1
@@ -11,12 +15,20 @@ const reminders = [
     "travel" //4
 ];
 
-const reminderFile = "reminders.txt";
+const dates = [
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+];
 
 // for loop 
-for(let i = 0; 1 < reminders.length; i += 1){
+for(let i = 0; i < reminders.length; i += 1){
     const reminder = reminders[i];
-    fs.appendFileSync(reminderFile, reminder + '\n');
+    const date = dates[i];
+    const line = `${reminder}|${date}\n`;
+    fs.appendFileSync(reminderFile, line);
 }
 
 console.log("Wrote all of the reminders to the file");
