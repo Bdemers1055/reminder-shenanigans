9f38aec09ebf1d745405b42e455cd6b39866c2d0
bdemers1055 <bdemers1055@gmail.com>
2018-06-15 19:23:04 -0400

    print reminders

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 
 const fs = require('fs'); //this tells node that we need this module (set of functions and attributes)
 
-console.log("welcome to remind me"); // this tells node we need this module (se of functions and attributes)
-
 const reminderFile = "reminders.txt";
-
-fs.unlinkSync(reminderFile);
 
 const reminders = [
     "get some milk", //0
@@ -23,12 +19,41 @@ const dates = [
     new Date(),
 ];
 
-// for loop 
-for(let i = 0; i < reminders.length; i += 1){
-    const reminder = reminders[i];
-    const date = dates[i];
-    const line = `${reminder}|${date}\n`;
-    fs.appendFileSync(reminderFile, line);
+// list command where all the reminders are printed
+// add a command where we can add a reminder
+const args = process.argv.slice(2);
+const subcommand = args[0];
+if(subcommand === 'list'){
+        list();
+    } else if (subcommand === 'add'){
+        add();
+    } else {
+        help();
 }
 
-console.log("Wrote all of the reminders to the file");
+console.log("Completed your request.");
+process.exit(0);
+
+function list(){
+    console.log('Here are the things you need to do:');
+        reminders.forEach((reminder, index) => {
+            const line = `-      ${reminder}    Due:${dates[index]}`;
+            console.log(line);
+        });
+}
+
+function add(){
+    console.log("add");
+    fs.unlinkSync(reminderFile);
+    // for loop 
+    for(let i = 0; i < reminders.length; i += 1){
+        const reminder = reminders[i];
+        const date = dates[i];
+        const line = `${reminder}|${date}\n`;
+        fs.appendFileSync(reminderFile, line);
+    }
+}
+
+function help(){
+    console.log('Something went wrong. Please check you syntax.');
+}
