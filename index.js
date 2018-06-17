7c58a185445364fa785e4a8a5fa778909f5ed9d0
bdemers1055 <bdemers1055@gmail.com>
2018-06-17 17:42:54 -0400

    changed the add subcommand to take arguments from the user to add to the file. Wrote new line to the file that was provided by the user.

--- index.js
+++ index.js
@@ -3,22 +3,6 @@ const fs = require('fs'); //this tells node that we need this module (set of fun
 
 const reminderFile = "reminders.txt";
 
-const reminders = [
-    "get some milk", //0
-    "write a song", //1
-    "join a team", //2 
-    "read a book", //3
-    "travel" //4
-];
-
-const dates = [
-    new Date(),
-    new Date(),
-    new Date(),
-    new Date(),
-    new Date(),
-];
-
 // list command where all the reminders are printed
 // add a command where we can add a reminder
 const args = process.argv.slice(2);
@@ -26,7 +10,8 @@ const subcommand = args[0];
 if(subcommand === 'list'){
         list();
     } else if (subcommand === 'add'){
-        add();
+        const reminder = args[1];
+        add(reminder);
     } else {
         help();
 }
@@ -34,12 +19,11 @@ if(subcommand === 'list'){
 console.log("Completed your request.");
 process.exit(0);
 
-
 // these are at the bottom purely for convention, functions hoist to the top 
 function list(){
     console.log('Here are the things you need to do:');
     const lines = fs.readFileSync(reminderFile, 'utf8').split('\n');
-    const parsedLines = lines.map(line => line.split("|");
+    const parsedLines = lines.map(line => line.split("|"));
     const humanFriendlyLines = parsedLines.map(parsedLine => {
         const reminder = parsedLine[0];
         const date = parsedLine[1];
@@ -47,36 +31,25 @@ function list(){
     });
     const output = humanFriendlyLines.join('\n');
     console.log(output);
-        // reminders.forEach((reminder, index) => {
-        //     const line = `-      ${reminder}    Due:${dates[index]}`;
-        //     console.log(line);
-        // });
 }
 
-function list(){
-    console.log('Here are the things that you need to do...');
+console.log("Completed your request.");
+process.exit(0);
+
+function add(addition){
+    console.log("Adding a new reminder ...");
     const lines = fs.readFileSync(reminderFile, 'utf8').split('\n');
     const parsedLines = lines.map(line => line.split("|"));
-    const humanFriendlyLines = parsedLines.map(parsedLine => {
-        const reminder = parsedLine[0];
-        const date = parsedLine[1];
-        return `-     ${reminder}           Due: ${date}`;
-    });
-    const output = humanFriendlyLines.join('\n');
-    console.log(output);
-}
-
-
-function add(){
-    console.log("add");
+    const newLine = [[addition, new Date()]];
+    const withAddition = parsedLines.concat(newLine);
     fs.unlinkSync(reminderFile);
-    // for loop 
-    for(let i = 0; i < reminders.length; i += 1){
-        const reminder = reminders[i];
-        const date = dates[i];
-        const line = `${reminder}|${date}\n`;
-        fs.appendFileSync(reminderFile, line);
-    }
+    const outputLines = withAddition.map(line => {
+        const reminder = line[0];
+        const date = line[1];
+        return `${reminder}|${date}`;
+    });
+    const output = outputLines.join(`\n`);
+    fs.appendFileSync(reminderFile, output);
 }
 
 function help(){
