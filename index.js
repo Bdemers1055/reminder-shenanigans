2b59f2df5c291325e875fded07e0a095913b2b05
bdemers1055 <bdemers1055@gmail.com>
2018-06-19 19:36:27 -0400

    move reminder list model to models folder

--- index.js
+++ index.js
@@ -2,38 +2,8 @@
 const fs = require('fs'); //this tells node that we need this module (set of functions and attributes)
 
 const Reminder = require('./models/Reminder');
+const ReminderList = require('./models/ReminderList');
 const reminderFile = "reminders.txt";
-
-class ReminderList {
-        constructor(filepath){
-            this.filepath = filepath;
-            const lines = fs.readFileSync(reminderFile, 'utf8').split('\n');
-            const parsedLines = lines.map(line => line.split("|"));
-            const humanFriendlyLines = parsedLines.map(parsedLine => {
-                const reminder = new Reminder(parsedLine[0], parsedLine[1]);
-                return reminder;
-        });
-        this.reminders = humanFriendlyLines;
-    }
-
-    add(reminder){
-        this.reminders.push(reminder);
-    }
-
-    toFileOutput(){
-        const fileString = this.reminders.map((reminder) => reminder.toFileString());
-        const output = fileString.join('\n');
-        return output;
-    }
-
-    toConsole(){
-        const reminderStrings = this.reminders.map((reminder) => reminder.toString());
-        const output = reminderStrings.join('\n');
-        console.log(output);
-    }
-}
-
-module.exports = ReminderList;
 
 // list command where all the reminders are printed
 // add a command where we can add a reminder
